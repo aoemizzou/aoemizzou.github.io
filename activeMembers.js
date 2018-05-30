94e2608fd579ac241da2b3567501dd1fe40f06ac
aoemizzou <30819803+aoemizzou@users.noreply.github.com>
2018-05-29 22:37:02 -0700

    Update activeMembers.js

--- activeMembers.js
+++ activeMembers.js
@@ -18,11 +18,59 @@ app.controller("myCtrl", function($scope) {
             "Name" : "Jamie Cottrell",
             "Major" : "Undecided"
        },{
-            "Name" : "Jessica Austin",
+            "Name" : "Olivia Decker",
+            "Major" : "Mechanical Engineering and Physics"
+       },{
+            "Name" : "Laura Deters",
+            "Major" : "Biological Engineering"
+       },{
+            "Name" : "Mojan Felfeli",
             "Major" : "Mechanical Engineering"
        },{
-            "Name" : "Jessica Austin",
+            "Name" : "Libby Freihaut",
             "Major" : "Mechanical Engineering"
+       },{
+            "Name" : "Martha Gahl",
+            "Major" : "Electrical Engineering and Physics"
+       },{
+            "Name" : "Madeline Harl",
+            "Major" : "Civil Engineering"
+       },{
+            "Name" : "Maria Howell",
+            "Major" : "Electrical Engineering and Physics"
+       },{
+            "Name" : "Tsiania Hughes",
+            "Major" : "Computer Engineering"
+       },{
+            "Name" : "Breanna Ladd",
+            "Major" : "Civil Engineering"
+       },{
+            "Name" : "Kiley Marshall",
+            "Major" : "Biological Engineering"
+       },{
+            "Name" : "Sidney McMillan",
+            "Major" : "Chemical Engineering"
+       },{
+            "Name" : "Hannah Moss",
+            "Major" : "Strategic Communications"
+       },{
+            "Name" : "MaryRose Ornosky",
+            "Major" : "Pre-Engineering"
+       },{
+            "Name" : "Kiley Marshall",
+            "Major" : "Biological Engineering"
+       },{
+            "Name" : "Kiley Marshall",
+            "Major" : "Biological Engineering"
+       },{
+            "Name" : "Kiley Marshall",
+            "Major" : "Biological Engineering"
+       },{
+            "Name" : "Kiley Marshall",
+            "Major" : "Biological Engineering"
+       },{
+            "Name" : "Kiley Marshall",
+            "Major" : "Biological Engineering"
        },{
             "Name" : "Sarah Twomey",
             "Major" : "Computer Science and Information Technology"
