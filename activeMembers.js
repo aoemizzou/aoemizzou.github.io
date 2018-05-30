3d68544e93d8c499fd1a9f78363e75abe0ad7dc4
aoemizzou <30819803+aoemizzou@users.noreply.github.com>
2018-05-29 21:22:21 -0700

    initial commit

---
+++ activeMembers.js
@@ -0,0 +1,19 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+
+    $scope.portfolio = [
+       {
+            "Name" : "Sarah Twomey",
+            "Major" : "Computer Science and Information Technology"
+       },{
+            "Name" : "Allison Weyhrich",
+            "Major" : "Mechanical Engineering"
+        },{
+            "Name" : "Kiersten York",
+            "Major" : "Mechanical Engineering"
+       {
+            "Name" : "Allison Weyhrich",
+            "Major" : "Mechanical Engineering"
+        }
+    ]
+});
