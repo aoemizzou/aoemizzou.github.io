2d69d0c3cd6c607c9cfa6ec583b35781b997f0f6
aoemizzou <30819803+aoemizzou@users.noreply.github.com>
2018-05-29 22:28:02 -0700

    Update activeMembers.js

--- activeMembers.js
+++ activeMembers.js
@@ -6,6 +6,24 @@ app.controller("myCtrl", function($scope) {
             "Name" : "Jessica Austin",
             "Major" : "Mechanical Engineering"
        },{
+            "Name" : "Lauren Baratta",
+            "Major" : "Chemical Engineering"
+       },{
+            "Name" : "Stacey Bonderer",
+            "Major" : "Civil Engineering"
+       },{
+            "Name" : "Megan Carlin",
+            "Major" : "Civil and Environnmetal Engineering"
+       },{
+            "Name" : "Jamie Cottrell",
+            "Major" : "Undecided"
+       },{
+            "Name" : "Jessica Austin",
+            "Major" : "Mechanical Engineering"
+       },{
+            "Name" : "Jessica Austin",
+            "Major" : "Mechanical Engineering"
+       },{
             "Name" : "Sarah Twomey",
             "Major" : "Computer Science and Information Technology"
        },{
